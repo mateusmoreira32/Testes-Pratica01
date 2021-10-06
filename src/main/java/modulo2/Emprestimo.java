5ec1f7cd4ed1dc08d344f3dbca1b2a47f958f248
mateusmoreira32 <74328621+mateusmoreira32@users.noreply.github.com>
2021-10-06 18:57:26 -0300

    Update Emprestimo.java

--- src/main/java/modulo2/Emprestimo.java
+++ src/main/java/modulo2/Emprestimo.java
@@ -11,4 +11,46 @@ public class Emprestimo {
   private Date dataEmprestimo;
   private Date dataDevolucao;
   private List<Livro> livros;
+  
+  public Emprestimo(){}
+  
+  public Emprestimo(Pessoa usuario, Date dataEmprestimo, Date dataDevolucao, List<Livro> livros) {
+        this.usuario = usuario;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+        this.livros = livros;
+    }
+
+    public Pessoa getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Pessoa usuario) {
+        this.usuario = usuario;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+  
 }
