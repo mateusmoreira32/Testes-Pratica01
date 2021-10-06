25d60dbf39a2d3f69706aee5fc3e61640cbea877
Higorcz32 <higorcz@hotmail.com>
2021-10-06 11:59:37 -0300

    Módulo 4

--- src/test/java/modulo4/LivroTest.java
+++ src/test/java/modulo4/LivroTest.java
@@ -1,4 +1,42 @@
 package modulo4;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
 public class LivroTest {
+
+    Livro livro;
+    private final Logger logger = Logger.getLogger("my-Logger");
+
+    @After
+    public void printMessage(){
+        this.logger.info("Início dos testes" );
+    }
+
+    @Test
+    public void verificaNome(){
+        livro = new Livro();
+        livro.setNome("Harry Potter");
+
+        Assert.assertEquals("Harry Potter", livro.getNome());
+    }
+
+    @Test
+    public void verificarAutor(){
+        livro = new Livro();
+        livro.setAutor("Higor");
+
+        Assert.assertEquals("Higor", livro.getAutor());
+    }
+
+    @Test
+    public void verificarCodigoValido(){
+        livro = new Livro();
+        livro.setCodigo("123");
+
+        Assert.assertNotNull(livro.getCodigo());
+    }
 }
